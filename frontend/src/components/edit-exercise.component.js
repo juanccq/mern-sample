86ce6c96bfff9927f645723d4dfb50372c610a04
Juan Carlos Choque Quispe <juan.c.c.q@gmail.com>
2020-12-18 20:34:49 -0400

    conexion on backend to mongodb and forms on frontend

---
+++ frontend/src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class EditExercises extends Component {
+  render() {
+    return (
+      <div>
+        <p>Your are in Edit Exercises component</p>
+      </div>
+    );
+  }
+}
---
+++ frontend/src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ExercisesList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Your are in Exercises List component</p>
+      </div>
+    );
+  }
+}
